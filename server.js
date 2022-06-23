8918f5c1cb6edac5a79195ffb6cfc796693dfe93
Ali <ali27muhammad56@gmail.com>
2022-06-23 13:19:00 +0500

    backend setup

---
+++ server.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+const axios = require("axios");
+const app = express();
+const Port = process.env.Port || 6058;
+app.use(cors());
+app.options("*", cors());
+app.use(express.json());
+
+const local = true;
+let credentials = {};
+
+if (local) {
+  credentials = {
+    key: fs.readFileSync("/etc/apache2/ssl/onlinetestingserver.key", "utf8"),
+    cert: fs.readFileSync("/etc/apache2/ssl/onlinetestingserver.crt", "utf8"),
+    ca: fs.readFileSync("/etc/apache2/ssl/onlinetestingserver.ca"),
+  };
+} else {
+  credentials = {
+    key: fs.readFileSync("../certs/ssl.key"),
+    cert: fs.readFileSync("../certs/ssl.crt"),
+    ca: fs.readFileSync("../certs/ca-bundle"),
+  };
+}
+
+app.get("/", (req, res) => {
+  res.send("Zip it solar is Running");
+});
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(Port, () => {
+  console.log(`Server is running at the port ${Port}`);
+});
+
+//for filtering the tesla stations
+app.use("/test", async (req, res) => {
+  try {
+    const { language, state, lat, long } = req.query;
+    let { data } = await axios({
+      method: "GET",
+      url: `${process.env.TESLA_API}lang=${language}&q=${state}&loc=${lat},${long}`,
+    });
+    // console.log("owner",data.)
+    data = data.filter((x) => x.operator == "Tesla");
+    return res.status(200).json({ data });
+  } catch (err) {
+    console.log("Error", err);
+    return res.status(500).json(err);
+  }
+});
